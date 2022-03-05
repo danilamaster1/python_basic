c995ea66266376ba62a3600fede7bdc45da3d1ea
big_sosa <howarddan11@mail.ru>
2022-03-06 02:15:58 +0300

    percentages

---
+++ Les_1/task_3.py
@@ -0,0 +1,7 @@
+for i in range(101):
+    if i % 10 == 1 and i % 100 != 11:
+        print(f'{i} процент')
+    elif 1 < i % 10 < 5 and not 11 < i % 100 < 15:
+        print(f'{i} процента')
+    else:
+        print(f'{i} процентов')
